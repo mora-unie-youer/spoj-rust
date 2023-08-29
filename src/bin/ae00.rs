8c358e73b4dca04efbeb9780a2f56b1d18a940c7
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-08-29 09:44:03 +0000

    Solution for `AE00`

---
+++ src/bin/ae00.rs
@@ -0,0 +1,37 @@
+use std::io::BufRead;
+
+/**
+ * AE00 - Rectangles
+ *
+ * Byteman has a collection of N squares with side 1. How many different rectangles can he form using these squares?
+ *
+ * Two rectangles are considered different if none of them can be rotated and moved to obtain the second one. During
+ * rectangle construction, Byteman can neither deform the squares nor put any squares upon any other ones.
+ *
+ * Input
+ * The first and only line of the standard input contains one integer N (1 <= N <= 10000).
+ *
+ * Output
+ * The first and only line of the standard output should contain a single integer equal to the number of different
+ * rectangles that Byteman can form using his squares.
+ *
+ * Example
+ *   For the input data:
+ *   6
+ *   
+ *   the correct result is:
+ *   8
+ */
+fn main() {
+    let stdin = std::io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let n: usize = lines.next().unwrap().unwrap().parse().unwrap();
+    let mut answer = 0;
+    for i in 1..=n {
+        let j = n / i;
+        answer += j.saturating_sub(i - 1);
+    }
+
+    println!("{}", answer);
+}
